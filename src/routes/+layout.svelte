7db730d137c0b17a4db15d1d5e66811f80cc0faf
VerburgtJimmy <jimmyverburgt@gmail.com>
2026-01-27 23:20:46 +0100

    Add favicon images and web manifest for JTransfer application

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
-  import favicon from "$lib/assets/favicon.svg";
   import { ModeWatcher, setMode } from "mode-watcher";
   import "../app.css";
   let { children } = $props();
-
-  setMode("light");
 </script>
 
 <svelte:head>
-  <link rel="icon" href={favicon} />
   <title>JTransfer - Secure File Sharing</title>
 </svelte:head>
 <ModeWatcher />
